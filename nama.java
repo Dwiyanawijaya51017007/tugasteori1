8ca2b4bb858adae43f8aa8ff65a9dda52b47a9f1
Dwiyanawijaya51017007 <48231241+Dwiyanawijaya51017007@users.noreply.github.com>
2019-09-18 17:36:34 +0800

    Add files via upload

---
+++ nama.java
@@ -0,0 +1,31 @@
+/**
+ * @(#)nama.java
+ *
+ *
+ * @author
+ * @version 1.00 2019/9/18
+ */
+
+import java.util.Scanner;
+public class nama {
+
+    public static void main(String[] args) {
+    	Scanner input = new Scanner(System.in);
+		String nama1, alamat;
+		int tahun;
+
+		System.out.print("Masukkan nama: ");
+		nama1=input.nextLine();
+		System.out.print("Masukkan alamat: ");
+		alamat=input.nextLine();
+		System.out.print("Masukkan umur: ");
+		tahun=input.nextInt();
+		System.out.println();
+
+		System.out.println("Saudara: "+nama1);
+		System.out.println("Tinggal di: "+alamat);
+		System.out.println("Berumur: "+tahun+" tahun");
+    }
+
+
+}
---
+++ teoritugas.java
@@ -0,0 +1,33 @@
+/**
+ * @(#)teoritugas.java
+ *
+ *
+ * @author
+ * @version 1.00 2019/9/18
+ */
+
+
+public class teoritugas {
+
+    public static void main (String[] args) {
+    	System.out.println("Hasil Yang Diperoleh");
+
+		int aa = 5;
+		int bb = 6;
+		double cc = 0.5;
+
+		double aabb=aa+bb;
+		double aabbcc=aa*bb*cc;
+		double bbcc=bb/cc;
+
+		System.out.println();
+		System.out.println("Nilai aa + bb = " +aabb);
+		System.out.println("Nilai aa x bb x cc = " +aabbcc);
+		System.out.println("Nilai bb / cc = " + bbcc);
+		System.out.println();
+
+
+    }
+
+
+}
